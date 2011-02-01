9610c3484ffbb71d300e4d8c1ba56be88f4cc912
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-02-01 11:36:55 +0100

    added German translation

--- project.py
+++ project.py
@@ -6,6 +6,6 @@ class project_viewchildren(osv.osv):
     _inherit = "project.project"
     
     _columns = {
-        "child_ids": fields.one2many("project.project", "parent_id", "Is parent to these projects"),
+        "child_ids": fields.one2many("project.project", "parent_id", "Child projects"),
     }
 project_viewchildren()
