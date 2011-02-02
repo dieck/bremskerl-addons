7325aa4d278aa71b5a6a28ef72826d19ebfc965b
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-02-02 11:35:02 +0100

    initial

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Show Purchase Order Lines menu entry",
+    "version": "1.01",
+    "depends": [ "purchase" ],
+    "author": "Marco Dieckhoff, BREMSKERL",
+    "category": "Custom",
+    "description": """Show Purchase Order Lines menu entry
+    """,
+    "init_xml": [],
+    'update_xml': ["view/purchase_view.xml"],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+#    'certificate': '${certificate}',
+}
