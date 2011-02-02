bf7598de9ddbaa563e5c25b13147ec42cc5bfe04
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-02-02 11:30:01 +0100

    initial

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Purchase Order View Currency",
+    "version": "1.0",
+    "depends": ["purchase"],
+    "author": "Marco Dieckhoff, BREMSKERL",
+    "category": "Generic Modules/Projects & Services",
+    "description": """
+    Adds currency to Purchase Order List view
+    """,
+    "init_xml": [],
+    'update_xml': [
+                   "view/purchase_view.xml",
+                   ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
