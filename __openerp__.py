0e9e5ade8483cc410dc94a98d1c3076413004f1b
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-02-08 14:45:29 +0100

    initial

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Add QA documentation type",
+    "version": "1.01",
+    "depends": ["product_qa"],
+    "author": "Marco Dieckhoff, BREMSKERL",
+    "category": "Inventory Control",
+    "description": """Add field for type of documentation (quality assurance)
+    """,
+    "init_xml": [],
+    'update_xml': ["view/product_view.xml"],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+#    'certificate': '${certificate}',
+}
---
+++ product.py
@@ -0,0 +1,12 @@
+
+from osv import osv, fields
+
+class product_qa_documentation_type_product(osv.osv):
+    _name = "product.product"
+    _inherit = "product.product"
+    
+    _columns = {
+        'qa_doctype': fields.char("Type of documentation", size=50),
+    }
+            
+product_qa_documentation_type_product()
