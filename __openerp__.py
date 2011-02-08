e6b49bbb6b3d1ead7a02d6dbb71acf3b16ff9e66
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-02-08 12:09:25 +0100

    initial

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Make product code unique",
+    "version": "1.01",
+    "depends": ["product"],
+    "author": "Marco Dieckhoff, BREMSKERL",
+    "category": "Inventory Control",
+    "description": """Makes product code (default_code) a unique field 
+    """,
+    "init_xml": [],
+    'update_xml': [],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+#    'certificate': '${certificate}',
+}
