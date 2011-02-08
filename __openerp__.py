6a0eefb08ead2407fb6e4caa25f31ec89cc0230e
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-02-08 15:13:31 +0100

    initial

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Add packaging description to list view",
+    "version": "1.01",
+    "depends": ["product"],
+    "author": "Marco Dieckhoff, BREMSKERL",
+    "category": "Inventory Control",
+    "description": """Add packaging description to list view
+    """,
+    "init_xml": [],
+    'update_xml': ["view/product_view.xml"],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+#    'certificate': '${certificate}',
+}
