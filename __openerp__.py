2c4c4e7865bec8cd6e691dd1c2363721f4cc1f15
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-02-08 14:46:20 +0100

    initial

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+    "name": "QA section in product information",
+    "version": "1.01",
+    "depends": ["product"],
+    "author": "Marco Dieckhoff, BREMSKERL",
+    "category": "Inventory Control",
+    "description": """Provides QA section in product information for multiple addons
+    """,
+    "init_xml": [],
+    'update_xml': ["view/product_view.xml"],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+#    'certificate': '${certificate}',
+}
