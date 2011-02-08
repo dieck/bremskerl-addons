bb33521cde83b5aacca12792d278ae090c7e505d
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-02-08 17:13:29 +0100

    initial

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Add external accounting references to partners",
+    "version": "1.01",
+    "depends": ["base"],
+    "author": "Marco Dieckhoff, BREMSKERL",
+    "category": "Base",
+    "description": """Add mandatory external accounting references to partners.
+    """,
+    "init_xml": [],
+    'update_xml': ["view/partner_view.xml"],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+#    'certificate': '${certificate}',
+}
