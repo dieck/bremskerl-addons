903e96bcbb0f725059f6f106dc4b780569cf28e2
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-02-15 11:38:32 +0100

    set copyright notice

--- __openerp__.py
+++ __openerp__.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP Module
+#    
+#    Copyright (C) 2010-2011 BREMSKERL-REIBBELAGWERKE EMMERLING GmbH & Co. KG
+#    Author Marco Dieckhoff
+#
+##############################################################################
 {
     "name": "Add QA documentation type",
     "version": "1.01",
