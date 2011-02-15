f21915208d594fc9d76de63b7dcdf0826caecd87
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-02-15 11:37:50 +0100

    set copyright notice

--- __openerp__.py
+++ __openerp__.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP Module
+#    
+#    Copyright (C) 2010-2011 BREMSKERL-REIBBELAGWERKE EMMERLING GmbH & Co. KG
+#    Author Marco Dieckhoff
+#
+##############################################################################
 {
     "name": "Make product code mandatory",
     "version": "1.01",
