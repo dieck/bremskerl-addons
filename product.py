66ff2e40b7a398ae066f4d718ddf85d5bf297bd6
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-03-14 09:06:48 +0100

    renamed constraint

--- product.py
+++ product.py
@@ -3,10 +3,10 @@
 from osv import osv, fields
 from tools.translate import _
 
-class product_uom_unique_uopm(osv.osv):
+class product_uom_unique_uom(osv.osv):
     _name = "product.uom"
     _inherit = "product.uom"
   
-    _sql_constraints = [ ('default_code_uniq', 'unique (name, category_id)', """UoM name has to be unique per category."""), ]
+    _sql_constraints = [ ('product_uom_name_uniq', 'unique (name, category_id)', """UoM name has to be unique per category."""), ]
         
-product_uom_unique_uopm
+product_uom_unique_uom
