1140d362d93dcc8a13a3e5ac631540222408e359
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-03-24 17:01:50 +0100

    Postgres doesn't manage unique constraints with possible null values well. Two nulls are always different, so unique key won't be bothered by this. Fixed/Worked around by using a python constraint.

--- product.py
+++ product.py
@@ -6,6 +6,24 @@ from tools.translate import _
 class product_code_unique_product(osv.osv):
     _name = "product.product"
     _inherit = "product.product"
+      
+    def _check_defaultcode_and_variants(self, cr, uid, ids):
+        for session in self.browse(cr, uid, ids):
+            res = self.search(cr, uid, [ ('default_code','=',session.default_code),
+                                         ('variants','=',session.variants)
+                                       ])
+            # Result will contain the current session, we remove it here.
+            res.remove( session.id )
+            if len(res):
+                # If we have any results left
+                # we have duplicate entries
+                return False
+        return True
+    
+    _constraints = [(_check_defaultcode_and_variants,
+                    _('Reference/Variant has to be unique.'),
+                    ['default_code','variants'])
+                   ]
   
     _sql_constraints = [ ('default_code_uniq', 'unique (default_code, variants)', """Reference/Variant has to be unique."""), ]
 
