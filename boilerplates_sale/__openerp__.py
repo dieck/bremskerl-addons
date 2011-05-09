9b5bef6f6e33ba312138a1403e8da652345f3f92
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-05-09 15:10:39 +0200

    wizard for adding notes to sale orders

---
+++ boilerplates_sale/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP Module
+#    
+#    Copyright (C) 2010-2011 BREMSKERL-REIBBELAGWERKE EMMERLING GmbH & Co. KG
+#    Author Marco Dieckhoff
+#
+##############################################################################
+#
+#    Merge Picking up to v5 of OpenERP was written by Axelor, www.axelor.com
+#    This is a complete rewrite.
+#
+##############################################################################
+{
+    'name' : 'Boilerplates for Sales',
+    'version' : '6.0',
+    'author' : 'Marco Dieckhoff (Bremskerl)',
+    'website' : 'www.bremskerl.com',
+    'depends' : ['boilerplates','sale'],
+    'category' : 'Boilerplates',
+    'description': "TODO",
+    'init_xml' : [],
+    'demo_xml' : [],
+    'update_xml' : ['wizard/boilerplates_sale_view.xml'],
+    'active': False,
+    'installable': True
+}
