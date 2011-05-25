6f08af25bf062f481443c2383f9de4f742437782
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-05-25 16:24:48 +0200

    mvoed from relation to stored function, to have it available for search&order

--- stock_picking_clientorderref/stock.py
+++ stock_picking_clientorderref/stock.py
@@ -5,8 +5,21 @@ class stock_picking_clientorderref(osv.osv):
     _name = "stock.picking"
     _inherit = "stock.picking"
     
+    def _get_sale_client_order_ref(self, cr, uid, ids, field_name, arg, context={}):
+        res = {}
+        for session in self.browse(cr,uid,ids,context=context):
+            if (session.sale_id):
+                res[session.id] = session.sale_id.client_order_ref or False
+        return res
+
+    def _trigger_sale_client_order_ref(self, cr, uid, ids, context={}):
+        return self.search(cr, uid, [('sale_id','in',ids)])
+        
+    
     _columns = {
-         'sale_client_order_ref' : fields.related('sale_id', 'client_order_ref', type='char', size=64, string='Sales Order Customer Reference'),
+        "sale_client_order_ref": fields.function(_get_sale_client_order_ref, type='char', size=64,
+                                                 method=True, string="Sales Order Customer Reference", 
+                                                 store={'sale.order':(_trigger_sale_client_order_ref,['client_order_ref'],10)})
     }
     
 stock_picking_clientorderref()
