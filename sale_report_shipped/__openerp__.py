b8566ec074952999531e7148f436572bfed01f3a
Marco Dieckhoff <marco.dieckhoff@bremskerl.de>
2011-09-20 13:03:18 +0200

    developed extended sales report (with shipped quantities)

---
+++ sale_report_shipped/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP Module
+#    
+#    Copyright (C) 2010-2011 BREMSKERL-REIBBELAGWERKE EMMERLING GmbH & Co. KG
+#    Author Marco Dieckhoff
+#
+##############################################################################
+# based on sale/report/sale_report from openobject-addons/6.0
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+##############################################################################
+
+{
+    'name' : 'Sales Report with Shipped Quantities',
+    'version' : '6.0',
+    'author' : 'Marco Dieckhoff (Bremskerl)',
+    'website' : 'www.bremskerl.com',
+    'depends' : ['sale'],
+    'category' : 'Sales',
+    'description': """
+This module adds reporting for Sales with summed shipped quantities.
+    """,
+    'init_xml' : [],
+    'demo_xml' : [],
+    'update_xml' : ['sale_report_view.xml'],
+    'active': False,
+    'installable': True
+}
