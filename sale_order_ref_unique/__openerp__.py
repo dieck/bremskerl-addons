05149b8510dec8daac09aaef2737bf36109ac568
Marco Dieckhoff <dieckhoff@bremskerl.de>
2013-02-06 10:26:43 +0100

    added sale_order_ref_unique -- Sales Orders: Customer Reference will be unique per partner

---
+++ sale_order_ref_unique/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP Module
+#    
+#    Copyright (C) 2012+ BREMSKERL-REIBBELAGWERKE EMMERLING GmbH & Co. KG
+#    Author Marco Dieckhoff
+#
+##############################################################################
+{
+    "name": "Sales Orders: Customer Reference will be unique",
+    "version": "6.0.3",
+    "depends": ["sale"],
+    "author": "Marco Dieckhoff, BREMSKERL",
+    "category": "Sales",
+    "description": "Sales Orders: Customer Reference will be unique",
+    "init_xml": [],
+    'update_xml': [],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+#    'certificate': '${certificate}',
+}
