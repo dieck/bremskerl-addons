13592746f7335a106b74c4823f4868a494287530
Marco Dieckhoff <dieckhoff@bremskerl.de>
2013-02-06 10:26:11 +0100

    Problems with store policy trigger in stock_picking_clientorderref

--- stock_picking_clientorderref/stock.py
+++ stock_picking_clientorderref/stock.py
@@ -12,14 +12,31 @@ class stock_picking_clientorderref(osv.osv):
                 res[session.id] = session.sale_id and session.sale_id.client_order_ref or False
         return res
 
-    def _trigger_sale_client_order_ref(self, cr, uid, ids, context={}):
-        return self.search(cr, uid, [('sale_id','in',ids)])
-        
+
+    ##TODO this does not work and you get a "Key error: sale_id".
+    ## using store={'sale.order':(_trigger_sale_client_order_ref,['client_order_ref'],10)}
+    def DOESNOTWORK_trigger_sale_client_order_ref(self, cr, uid, ids, context={}):
+        return self.search(cr, uid, [('sale_id','IN',ids)])
     
+    ##TODO this does not work and shows an SQL error, sale_order.sale_id does not exist
+    ## but WHY sale_order? self.search should apply to stock.picking here?
+    ## using store={'sale.order':(_trigger_sale_client_order_ref,['client_order_ref'],10)}
+    def DOESNOTWORKEITHER_trigger_sale_client_order_ref(self, cr, uid, ids, context={}):
+        dom = []
+        for id in ids:
+            dom.append( ('sale_id','=',id) )
+        
+        # make "or" domain
+        for i in range(len(dom)-1):
+            dom.insert(0, '|')
+                            
+        return self.search(cr, uid, dom)
+
+  
     _columns = {
         "sale_client_order_ref": fields.function(_get_sale_client_order_ref, type='char', size=64,
                                                  method=True, string="Sales Order Customer Reference", 
-                                                 store={'sale.order':(_trigger_sale_client_order_ref,['client_order_ref'],10)})
+                                                 store=True)
     }
     
 stock_picking_clientorderref()
